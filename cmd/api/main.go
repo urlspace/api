32ad12431a88f22d96f19354fe0f92b69d398d9a
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-10-23 12:15:52 +0100

    remove api from the gitignore

---
+++ cmd/api/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/zapi-sh/api/internal/handlers"
+	"github.com/zapi-sh/api/internal/middlewares"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /status", handlers.Status)
+
+	handler := middlewares.CommonHeaders(mux)
+
+	s := &http.Server{
+		Addr:              ":" + port,
+		Handler:           handler,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       120 * time.Second,
+	}
+
+	log.Printf("Starting server on %s", port)
+
+	log.Fatal(s.ListenAndServe())
+}

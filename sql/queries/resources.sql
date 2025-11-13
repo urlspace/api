a6c8b1c1e1d3ef3ca90a149433d99b93786a8de4
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-11-13 18:12:16 +0000

    initial setup of sqlc

---
+++ sql/queries/resources.sql
@@ -0,0 +1,26 @@
+-- name: GetResource :one
+SELECT * FROM resources
+WHERE id = $1 LIMIT 1;
+
+-- name: ListResources :many
+SELECT * FROM resources
+ORDER BY title;
+
+-- name: CreateResource :one
+INSERT INTO resources (
+  title, url
+) VALUES (
+  $1, $2
+)
+RETURNING *;
+
+-- name: UpdateResource :one
+UPDATE resources
+  set title = $2,
+  url = $3
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteResource :exec
+DELETE FROM resources
+WHERE id = $1;
---
+++ sql/schema/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE resources (
+  id   BIGSERIAL PRIMARY KEY,
+  title text      NOT NULL,
+  url text      NOT NULL
+);

8d5f274f94796eac742dee7c79f36b7413e6e870
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-12-16 15:26:08 +0530

    fix verification token type on the db

--- sql/migrations/000001_init.up.sql
+++ sql/migrations/000001_init.up.sql
@@ -14,7 +14,7 @@ CREATE TABLE users (
 id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
 email TEXT NOT NULL UNIQUE,
 email_verified BOOLEAN NOT NULL DEFAULT FALSE,
-email_verification_token TEXT,
+email_verification_token UUID,
 email_verification_token_expires_at timestamptz,
 password TEXT NOT NULL,
 username TEXT NOT NULL UNIQUE,
